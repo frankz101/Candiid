ce1ac3393810d87cea250fafadb23b1445e0133c
andyren12 <andyren33@gmail.com>
2024-05-20 13:27:31 -0700

    tab bar frontend

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,12 +1,46 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const Layout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ tabBarLabel: "Home" }} />
-      <Tabs.Screen name="profile" options={{ tabBarLabel: "Profile" }} />
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#121212",
+          borderTopWidth: 0,
+          paddingHorizontal: wp(7),
+        },
+        tabBarItemStyle: {
+          alignItems: "center",
+          paddingTop: hp(1.2),
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "index") {
+            iconName = "home-outline";
+          } else if (route.name === "camera") {
+            iconName = "camera-outline";
+          } else if (route.name === "profile") {
+            iconName = "person-outline";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
+      })}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="camera" />
+      <Tabs.Screen name="profile" />
     </Tabs>
   );
 };
---
+++ frontend/app/(tabs)/camera.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Camera = () => {
+  <SafeAreaView>camera</SafeAreaView>;
+};
+
+export default Camera;
