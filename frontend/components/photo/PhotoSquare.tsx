ab3977a91cb453d669b436e0cc6583a96a6ada5a
Andy Ren <andyren33@gmail.com>
2024-07-27 13:19:02 -0400

    fix scroll on sharedalbum screen

--- frontend/components/photo/PhotoSquare.tsx
+++ frontend/components/photo/PhotoSquare.tsx
@@ -1,3 +1,4 @@
+import React, { useRef, useState } from "react";
 import {
   Alert,
   Animated,
@@ -6,11 +7,8 @@ import {
   Pressable,
   StyleSheet,
   Text,
-  TouchableOpacity,
-  Vibration,
   View,
 } from "react-native";
-import React, { useRef, useState } from "react";
 import { Image } from "expo-image";
 import { Ionicons } from "@expo/vector-icons";
 import { Link, useRouter } from "expo-router";
@@ -24,6 +22,11 @@ import {
 } from "react-native-responsive-screen";
 import * as Haptics from "expo-haptics";
 import * as MediaLibrary from "expo-media-library";
+import {
+  LongPressGestureHandler,
+  TapGestureHandler,
+  State,
+} from "react-native-gesture-handler";
 
 const screenWidth = Dimensions.get("window").width;
 const padding = 2;
@@ -47,9 +50,6 @@ const PhotoSquareSelect: React.FC<PhotoSquareSelectProps> = ({
   const [longPressModalVisible, setLongPressModalVisible] = useState(false);
   const [shortPressModalVisible, setShortPressModalVisible] = useState(false);
   const scaleValue = useRef(new Animated.Value(1)).current;
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-  const isLongPressRef = useRef(false);
-  const LONG_PRESS_DURATION = 300;
 
   const savePhoto = async () => {
     const { status } = await MediaLibrary.requestPermissionsAsync();
@@ -109,7 +109,6 @@ const PhotoSquareSelect: React.FC<PhotoSquareSelectProps> = ({
   };
 
   const openModal = (isLongPress: boolean) => {
-    isLongPressRef.current = isLongPress;
     isLongPress && Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     if (isLongPress) {
       setLongPressModalVisible(true);
@@ -126,146 +125,144 @@ const PhotoSquareSelect: React.FC<PhotoSquareSelectProps> = ({
     }
   };
 
-  const handlePressIn = () => {
-    Animated.spring(scaleValue, {
-      toValue: 1.08,
-      friction: 3,
-      useNativeDriver: true,
-    }).start();
-
-    isLongPressRef.current = false;
-
-    timerRef.current = setTimeout(() => {
-      isLongPressRef.current = true;
+  const handleLongPress = ({ nativeEvent }: any) => {
+    if (nativeEvent.state === State.BEGAN) {
+      Animated.spring(scaleValue, {
+        toValue: 1.08,
+        friction: 3,
+        useNativeDriver: true,
+      }).start();
+    } else if (nativeEvent.state === State.ACTIVE) {
       openModal(true);
-    }, LONG_PRESS_DURATION);
+    } else if (
+      nativeEvent.state === State.END ||
+      nativeEvent.state === State.CANCELLED
+    ) {
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        friction: 3,
+        useNativeDriver: true,
+      }).start();
+    }
   };
 
-  const handlePressOut = () => {
+  const handleTap = ({ nativeEvent }: any) => {
     Animated.spring(scaleValue, {
-      toValue: 1, // Scale back to 1
+      toValue: 1,
       friction: 3,
       useNativeDriver: true,
     }).start();
-
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-      timerRef.current = null;
-    } else {
-      return;
-    }
-
-    if (!isLongPressRef.current) {
+    if (nativeEvent.state === State.END) {
       openModal(false);
     }
   };
 
   return (
     <View style={styles.imageContainer}>
-      <Link
-        href={{
-          pathname: "/(hangout)/FullScreenImage",
-          params: { imageUrl, index },
-        }}
-        asChild
+      <LongPressGestureHandler
+        onHandlerStateChange={handleLongPress}
+        minDurationMs={200}
       >
-        <View>
-          <Pressable onPressIn={handlePressIn} onPressOut={handlePressOut}>
-            <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
-              <Image
-                source={{
-                  uri: imageUrl,
-                }}
-                style={styles.image}
-              />
-            </Animated.View>
-          </Pressable>
+        <TapGestureHandler onHandlerStateChange={handleTap}>
+          <Animated.View style={{ transform: [{ scale: scaleValue }] }}>
+            <Image
+              source={{
+                uri: imageUrl,
+              }}
+              style={styles.image}
+            />
 
-          {isLongPressRef.current ? (
-            <Modal
-              animationType="fade"
-              transparent={true}
-              visible={longPressModalVisible}
-            >
-              <Pressable style={styles.overlay} onPress={closeModal}>
-                <BlurView style={styles.modalContainer} intensity={20}>
-                  <View style={styles.modalContent}>
+            {longPressModalVisible && (
+              <Modal
+                animationType="fade"
+                transparent={true}
+                visible={longPressModalVisible}
+              >
+                <Pressable style={styles.overlay} onPress={closeModal}>
+                  <BlurView style={styles.modalContainer} intensity={20}>
+                    <View style={styles.modalContent}>
+                      <Image
+                        source={{
+                          uri: imageUrl,
+                        }}
+                        style={styles.magnifiedImage}
+                      />
+                      <View style={styles.buttonContainer}>
+                        <Pressable
+                          style={[
+                            styles.button,
+                            takenBy === user?.id && {
+                              borderBottomWidth: 1,
+                              borderBottomColor: "#3a3a3d",
+                            },
+                          ]}
+                          onPress={savePhoto}
+                        >
+                          <Text
+                            style={[
+                              styles.buttonText,
+                              {
+                                color: "white",
+                              },
+                            ]}
+                          >
+                            Save
+                          </Text>
+                          <Ionicons
+                            name="download-outline"
+                            size={24}
+                            color={"white"}
+                          />
+                        </Pressable>
+                        {takenBy === user?.id && (
+                          <Pressable
+                            style={styles.button}
+                            onPress={deletePhoto}
+                          >
+                            <Text style={[styles.buttonText, { color: "red" }]}>
+                              Delete
+                            </Text>
+                            <Ionicons
+                              name="trash-outline"
+                              size={24}
+                              color={"red"}
+                            />
+                          </Pressable>
+                        )}
+                      </View>
+                    </View>
+                  </BlurView>
+                </Pressable>
+              </Modal>
+            )}
+
+            {shortPressModalVisible && (
+              <Modal
+                animationType="fade"
+                transparent={true}
+                visible={shortPressModalVisible}
+              >
+                <Pressable
+                  style={[styles.overlay, { backgroundColor: "black" }]}
+                  onPress={closeModal}
+                >
+                  <View style={styles.fullScreenContainer}>
                     <Image
                       source={{
                         uri: imageUrl,
                       }}
-                      style={styles.magnifiedImage}
+                      style={{
+                        width: "100%",
+                        aspectRatio: 1,
+                      }}
                     />
-                    <View style={styles.buttonContainer}>
-                      <Pressable
-                        style={[
-                          styles.button,
-                          takenBy === user?.id && {
-                            borderBottomWidth: 1,
-                            borderBottomColor: "#3a3a3d",
-                          },
-                        ]}
-                        onPress={savePhoto}
-                      >
-                        <Text
-                          style={[
-                            styles.buttonText,
-                            {
-                              color: "white",
-                            },
-                          ]}
-                        >
-                          Save
-                        </Text>
-                        <Ionicons
-                          name="download-outline"
-                          size={24}
-                          color={"white"}
-                        />
-                      </Pressable>
-                      {takenBy === user?.id && (
-                        <Pressable style={styles.button} onPress={deletePhoto}>
-                          <Text style={[styles.buttonText, { color: "red" }]}>
-                            Delete
-                          </Text>
-                          <Ionicons
-                            name="trash-outline"
-                            size={24}
-                            color={"red"}
-                          />
-                        </Pressable>
-                      )}
-                    </View>
                   </View>
-                </BlurView>
-              </Pressable>
-            </Modal>
-          ) : (
-            <Modal
-              animationType="fade"
-              transparent={true}
-              visible={shortPressModalVisible}
-            >
-              <Pressable
-                style={[styles.overlay, { backgroundColor: "black" }]}
-                onPress={closeModal}
-              >
-                <View style={styles.fullScreenContainer}>
-                  <Image
-                    source={{
-                      uri: imageUrl,
-                    }}
-                    style={{
-                      width: "100%",
-                      aspectRatio: 1,
-                    }}
-                  />
-                </View>
-              </Pressable>
-            </Modal>
-          )}
-        </View>
-      </Link>
+                </Pressable>
+              </Modal>
+            )}
+          </Animated.View>
+        </TapGestureHandler>
+      </LongPressGestureHandler>
     </View>
   );
 };
